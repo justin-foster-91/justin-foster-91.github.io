34a5824f6c9c6253c2f7e991bc4f8ba2c9cd93a9
Justin Foster <jfoster91@gmail.com>
2021-06-30 17:24:23 -0600

    Initial commit

---
+++ precache-manifest.615aa2ef5f65ccde4cd594cfb6232e74.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97aa6b989d27462d5432f8dd1655e13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919ae3674003d75ee0c8",
+    "url": "/static/css/main.015e39d6.chunk.css"
+  },
+  {
+    "revision": "35d7937d82b8958bb417",
+    "url": "/static/js/2.1395b178.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/static/js/2.1395b178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919ae3674003d75ee0c8",
+    "url": "/static/js/main.3926472c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "64b7e553ed8c10982d83fe55dd1c7269",
+    "url": "/static/media/myAvatar.64b7e553.png"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
