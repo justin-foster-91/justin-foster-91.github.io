b7124f6d9d476cad3800f30070e04ec12c24d59d
Justin Foster <jfoster91@gmail.com>
2021-07-26 16:26:47 -0600

    Typo fixes

---
+++ precache-manifest.594b8d67d73f27ca99e09c3978186c35.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b70866a8a353e3106b717a0d10e60f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db66274d86564aaf7adf",
+    "url": "/static/css/main.1df14f82.chunk.css"
+  },
+  {
+    "revision": "49c41b695b536b5d87b2",
+    "url": "/static/js/2.ff368b78.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/static/js/2.ff368b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db66274d86564aaf7adf",
+    "url": "/static/js/main.7418c5ac.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "765e4e8f717f695939fdea4c5af804c3",
+    "url": "/static/media/CodeSpells.765e4e8f.png"
+  },
+  {
+    "revision": "ae1d003be92e954327c94d50d84100d9",
+    "url": "/static/media/GOAT.ae1d003b.png"
+  },
+  {
+    "revision": "9dca26bb494b2b36609463c028ce70ef",
+    "url": "/static/media/Logbook.9dca26bb.png"
+  },
+  {
+    "revision": "d9b78851f5f0bf1c403a559918b82cb2",
+    "url": "/static/media/Spaced_Repetition.d9b78851.jpg"
+  },
+  {
+    "revision": "3f1787381e573964c4752b32e4ca3f34",
+    "url": "/static/media/breakout.3f178738.png"
+  },
+  {
+    "revision": "fa4ceab1baa01b2a7164b1fbf2ee8dc8",
+    "url": "/static/media/desk-typing-filter.fa4ceab1.png"
+  },
+  {
+    "revision": "f5c3bdd0b4922dc612d6af962c398a4d",
+    "url": "/static/media/me.f5c3bdd0.jpeg"
+  },
+  {
+    "revision": "64b7e553ed8c10982d83fe55dd1c7269",
+    "url": "/static/media/myAvatar.64b7e553.png"
+  },
+  {
+    "revision": "212ad00bace40bc8fd466eb434bfcd2d",
+    "url": "/static/media/nexus.212ad00b.png"
+  }
+]);
